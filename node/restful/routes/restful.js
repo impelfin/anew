4ad62c3ee0b78e205099dd57e9dacdc09b97fc91
impelfin <impelfin@gmail.com>
2022-12-02 03:00:35 +0000

    restful

---
+++ node/restful/routes/restful.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+const users = [
+  { id:1, name: "User1"},
+  { id:2, name: "User2"},
+  { id:3, name: "User3"}
+];
+
+// simple api
+app.get("/Hello", (req, res) => {
+  res.send("Hello World!!");
+});
+
+// request param X, response O
+app.get("/api/users", (req, res) => {
+    res.json({ ok:true, users: users })
+})
+
+// Query param, request param O, response O
+app.get("/api/users/user", (req, res) => {
+    const user_id = req.query.user_id
+    const user = users.filter(data => data.id == user_id)
+    res.json({ ok:false, users: user })
+})
+
+// Path param, request param O, response O
+app.get("/api/users/:user_id", (req, res) => {
+    const user_id = req.params.user_id
+    const user = users.filter(data => data.id == user_id)
+    res.json({ ok:false, users: user })
+})
+
+module.exports = app;
